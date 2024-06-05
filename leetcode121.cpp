db6a75800a16345a7a3f2d414b9cd0464351686e
sagins <iamemrehansagin09@gmail.com>
2024-06-05 17:35:30 +0200

    solved 121

--- leetcode121.cpp
+++ leetcode121.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+
+        // track the minimum price and the maximum profit
+        // for each day, calculate the profit and update the minimum price
+        // if the profit is greater than the maximum profit, update the maximum profit
         
+        int curProf = 0;
+        int profit = 0;
+        int min = prices[0];
+
+        for (int i = 1; i < prices.size(); i++){
+            curProf = prices[i] - min;
+            if (prices[i] < min){
+                min = prices[i];
+            }
+            if (curProf > profit){
+                profit = curProf;
+            }
+        }
+
+        if (profit < 0) return 0;
+        return profit;
+
     }
 };
